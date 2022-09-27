a0b6c2670de744829e96da64b2adaf5c5c19598f
omayor18 <oejaramillo@stefanini.com>
2022-09-27 01:57:17 -0500

    Jwt config and token create

---
+++ src/main/java/com/mycompany/jwtdemo/config/JwtConfig.java
@@ -0,0 +1,54 @@
+package com.mycompany.jwtdemo.config;
+
+import com.mycompany.jwtdemo.service.CustomUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class JwtConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
+
+    //aqui decimos como queremos gestionar nuestro proceso de autenticación
+    //here we say how we want to manage our authentication process
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailService);
+    }
+
+    // Con este método controlaremos qué puntos finales están permitidos y cuáles no.
+    // whit this method we will controll which endpoints are permitted and which are not permitted
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .cors()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/api/generateToken").permitAll()//only allow this endpoint without authentication// permitir solo este punto final sin autenticación
+                .anyRequest().authenticated()//for any other request, authentication should performed //para cualquier otra solicitud, la autenticación debe realizarse
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//every request should be independent of other and server does not have to manage session
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder (){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
+    }
+}
